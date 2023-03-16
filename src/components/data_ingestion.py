5173bc25e55218579a9778fcd2f7df35c4137b41
Bakhtiar Rasheed <bakhtiar.baloch55@hotmail.com>
2023-03-16 23:27:28 +0500

    Data Ingestion Implementation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,2 +1,54 @@
 #Read data from a source
 #after ingesting, transform data
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    #Paths where the different data will be stored after retreiving from a source
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+
+class DataIngestion:
+    def __init__(self) :
+        self.ingestion_config=DataIngestionConfig()#variable containing all the saved data paths
+    def initiate_data_ingestion(self):
+        #read the dataset
+        logging.info("Entered the data ingestion method or component")
+
+        try:
+            df=pd.read_csv("notebook\data\stud.csv")
+            logging.info("Read the dataset as a dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            #save the raw data
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            logging.info("Train test split initiated")  
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+
+
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
-import logging
-import logger
+from src.logger import logging
+
 
 
 def error_message_detail(error,error_detail:sys):
