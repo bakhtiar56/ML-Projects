57b9d7cfa1df08cb466d4ae8f07748e3691ad5dd
Bakhtiar Rasheed <bakhtiar.baloch55@hotmail.com>
2023-03-26 14:50:48 +0500

    Finished Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,6 +17,8 @@ from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from  src.logger import logging
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
+
 
 
 @dataclass
@@ -64,3 +66,7 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -15,6 +16,26 @@ def save_object(filepath,obj):
 
         with open(filepath,"wb") as f:
             dill.dump(obj, f)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+
+
+
 
     except Exception as e:
         raise CustomException(e,sys)
